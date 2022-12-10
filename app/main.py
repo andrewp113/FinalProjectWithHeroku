2453907cb9565f7bf3cfb90acfe002bed3364248
andrewp113 <asp113@georgetown.edu>
2022-12-09 22:43:13 -0500

    Fixing files

--- app/main.py
+++ app/main.py
@@ -10,19 +10,15 @@ import spotipy
 import spotipy.util as util
 import app.tajMusic as tm
 
-
 # Spotify Token Access
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client_id = "dac824fa6ceb4b3483acc11595ea91d2"
+client_id = os.getenv("client_id") 
+client_secret = os.getenv("client_secret")
 
-client_secret = "a08528b1567b41d497c7ce06cf1e2295"
-
-# client_id = os.getenv("client_id") 
-# client_secret = os.getenv("client_secret")
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
